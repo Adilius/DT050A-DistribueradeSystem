9d3d1f90c9d1684d0d54e6751fe0d7c139333c9a
Adilius <leugue-gamer@hotmail.com>
2020-09-09 11:03:54 +0200

    added join and leave function

--- Laboration1/src/WindowProgram.java
+++ Laboration1/src/WindowProgram.java
@@ -1,18 +1,18 @@
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+import java.util.ArrayList;
 
 import se.miun.distsys.GroupCommuncation;
 import se.miun.distsys.listeners.ChatMessageListener;
 import se.miun.distsys.messages.ChatMessage;
 
+import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.util.Map;
 
 import javax.swing.JScrollPane;
 
@@ -25,7 +25,12 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 	JTextPane txtpnMessage = new JTextPane();
 	JTextPane txtpnClients = new JTextPane();
 	
-	GroupCommuncation gc = null;	
+	GroupCommuncation gc = null;
+
+	ArrayList<String> ClientList = new ArrayList<String>();
+
+	String JoinMessage = new String(" |joined the group|");
+	String LeftMessage = new String(" |left the group|");
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -75,12 +80,13 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 		//Set clients text pane
 		JScrollPane scrollPaneClients = new JScrollPane();
 		frame.getContentPane().add(scrollPaneClients);
+		scrollPaneClients.setViewportView(txtpnClients);
 		txtpnClients.setEditable(false);
 
 		//Window close
 		frame.addWindowListener(new java.awt.event.WindowAdapter() {
 	        public void windowClosing(WindowEvent winEvt) {
-				gc.sendChatMessage(" leaved the group.", true);
+				gc.sendChatMessage(LeftMessage, true);
 	            gc.shutdown();
 	        }
 	    });
@@ -88,20 +94,43 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 		//Window open
 		frame.addWindowListener(new java.awt.event.WindowAdapter(){
 			public void windowOpened(WindowEvent winEvt){
-				gc.sendChatMessage(" joined the group.", true);
+				gc.sendChatMessage(JoinMessage, true);
 			}
 		});
 	}
 
+	//Send message action
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		if (event.getActionCommand().equalsIgnoreCase("send")) {
 			gc.sendChatMessage(txtpnMessage.getText() , false);
 		}		
 	}
-	
+
+	//Get message action
 	@Override
 	public void onIncomingChatMessage(ChatMessage chatMessage) {	
-		txtpnChat.setText(chatMessage.chat + "\n" + txtpnChat.getText());				
+		txtpnChat.setText(chatMessage.chat + "\n" + txtpnChat.getText());
+
+		//If join message detected and is valid
+		if(chatMessage.chat.contains(JoinMessage) && !chatMessage.chat.contains(":")){
+
+			ClientList.add(chatMessage.chat.split(" ")[0]);
+			System.out.println("Client joined: " + ClientList.get(ClientList.size() - 1));
+			txtpnClients.setText("");
+			for(String client : ClientList){
+				txtpnClients.setText(client + "\n" + txtpnClients.getText());
+			}
+		}
+
+		//If leave message detected and is valid
+		if(chatMessage.chat.contains(LeftMessage) && !chatMessage.chat.contains(":")){
+
+			ClientList.remove(chatMessage.chat.split(" ")[0]);
+			txtpnClients.setText("");
+			for(String client : ClientList){
+				txtpnClients.setText(client + "\n" + txtpnClients.getText());
+			}
+		}
 	}
 }
